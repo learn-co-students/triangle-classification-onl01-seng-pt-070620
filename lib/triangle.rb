735d9ada435610b4431aad6f706259d723917e4f
Austin Stanley <stanley.377@buckeyemail.osu.edu>
2020-07-31 05:30:04 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
 class Triangle
-  # write code here
+  attr_accessor :side1, :side2, :side3
+  @all_sides = []
+  
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+    @all_sides = [side1, side2, side3]
+  end
+  
+  def valid?
+    @side1 + @side2 > @side3 && @side2 + @side3 > @side1 && @side1 + @side3 > @side2
+  end
+  
+  def non_triangle?
+    @all_sides.each do |length|
+      if length <= 0
+        return true
+      end
+    end
+  end
+
+  def kind
+    if non_triangle? == true || valid? == false
+      raise TriangleError
+    elsif @side1 != @side2 && @side1 != @side3 && @side2 != @side3
+      :scalene
+    elsif @side1 == @side2 && @side1 == @side3
+      :equilateral
+    elsif @side1 == @side2 || @side2 == @side3 || @side3 == @side1
+      :isosceles
+    end
+  end
+  class TriangleError < StandardError
+  end
 end
