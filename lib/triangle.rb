79ad68e4ec9f1b535b887a9fdc03a38665c86f29
Cristian Deleon <libaration@gmail.com>
2020-07-31 22:41:01 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,24 @@
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    total = [(a + b > c), (a + c > b), (b + c > a)]
+    raise TriangleError if total.include?(false)
+
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
+    elsif a != b && b != c && a != c
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
