8ae3d96211c23522ff47e1b28a8949e83032e49f
Nasrin Rahimi <77.nasrin@gmail.com>
2020-08-05 21:17:33 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,42 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  
+  attr_reader :sides
+  
+  def initialize(first_side,second_side,third_side)
+    @sides = first_side , second_side , third_side
+  end
+  
+  def kind
+    raise TriangleError if zero_or_negative_side? || !triangle_inequality?
+    case distinct_side_lengths
+      when 3
+        :scalene
+      when 2
+        :isosceles
+      when 1
+        :equilateral
+      end
+  end
+  
+  def distinct_side_lengths
+    @sides.uniq.count
+  end
+  
+  def zero_or_negative_side?
+    @sides.any? {|side| side <= 0 }
+  end
+  
+  def triangle_inequality?
+    @sides[0] + @sides[1] > @sides[2] &&
+    @sides[0] + @sides[2] > @sides[1] &&
+    @sides[1] + @sides[2] > @sides[0]
+  end
+  
+  class TriangleError < StandardError
+  end
+  
 end
+
+
