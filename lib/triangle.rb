912fb8956cfeda76601e64e00b4bed7bc28d50cd
Ebo Lee <yiboli.coding@gmail.com>
2020-08-05 21:26:40 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
+require 'pry'
 class Triangle
-  # write code here
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    if @a + @b = @c
+        raise TriangleError
+    elsif @a <= 0 || @b <= 0 || @c <= 0
+  #      raise TriangleError
+    else
+
+      if @a == @b && @b == @c
+        return :equilateral
+      elsif @a == @b
+        :isosceles
+      else
+        :scalene
+      end
+    end
+  end
+
+  class TriangleError < StandardError
+#    def message
+#      "invalid triangle"
+  end
+
 end
