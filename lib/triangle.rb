d3293c4326745724357f16c428007ebab4ff053f
github username <jhernandez1692@gmail.com>
2020-08-07 21:13:17 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 class Triangle
 
   attr_accessor :side_1, :side_2, :side_3
@@ -9,17 +11,32 @@ class Triangle
   end
 
   def kind
-    if (@side_1 == @side_2) && (@side_2 == @side_3)
+    if (@side_1 == @side_2 && @side_3) && (@side_2 == @side_3 && @side_1) 
+      if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
+        raise TriangleError
+      elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
+        raise TriangleError
+      else
       :equilateral
+      end
     elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
+      if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
+        raise TriangleError
+      elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
+        raise TriangleError
+      else
       :isosceles
+      end
     elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
+      if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
+        raise TriangleError
+      elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
+        raise TriangleError
+      else
       :scalene
-    elsif (@side_1 <= 0) && (@side_2 <= 0) && (@side_3 <= 0)
-      raise TriangleError
-    elsif (@side_1+@side_2 >= @side_3) || (@side_1+@side_3 >= @side_2) || (@side_2+@side_3 >= @side_1)
-      raise TriangleError       
-    end
+      end  
+    end   
+    
   end
 
   class TriangleError < StandardError
