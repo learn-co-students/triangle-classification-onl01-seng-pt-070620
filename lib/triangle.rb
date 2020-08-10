3029040db68691ae0ee936e4d0b4f189849500cb
Doug Jones <dsjr1284@gmail.com>
2020-08-10 00:53:25 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,6 +7,14 @@ class Triangle
   end
 
   def kind
+    if @s1 == nil ||  @s2 == nil || @s3 == nil
+      raise TriangleError
+    elsif @s1 <= 0 || @s2 <= 0 || @s3 <= 0
+      raise TriangleError
+    elsif (@s1 + @s2) <= @s3 || (@s2 + @s3) <= @s1 || (@s3 + @s1) <= @s2
+      raise TriangleError
+    end
+
     if @s1 == @s2 && @s1 == @s3
       return :equilateral
     elsif @s1 == @s2 || @s2 == @s3 || @s1 == @s3
@@ -14,12 +22,8 @@ class Triangle
     else
       return :scalene
     end
-    if @s1 == nil &&  @s2 == nil && @s3 == nil
-      raise TriangleError
-    elsif @s1 > 0 && @s2 > 0 && @s3 >0
-      raise TriangleError
-    elsif
   end
+
 
   class TriangleError < StandardError
      def message
