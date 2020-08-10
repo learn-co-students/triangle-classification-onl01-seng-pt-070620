7d4242cd1b5b965196d9907a03d157236989cd08
Doug Jones <dsjr1284@gmail.com>
2020-08-10 00:27:20 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
 class Triangle
-  # write code here
+
+  def initialize(s1, s2, s3)
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
+  end
+
+  def kind
+    if @s1 == @s2 && @s1 == @s3
+      return :equilateral
+    elsif @s1 == @s2 || @s2 == @s3 || @s1 == @s3
+      return :isosceles
+    else
+      return :scalene
+    end
+    if @s1 == nil &&  @s2 == nil && @s3 == nil
+      raise TriangleError
+    elsif @s1 > 0 && @s2 > 0 && @s3 >0
+      raise TriangleError
+    elsif
+  end
+
+  class TriangleError < StandardError
+     def message
+       "Invalid Trinagle."
+     end
+   end
+
 end
