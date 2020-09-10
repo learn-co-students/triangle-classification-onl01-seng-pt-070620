2b5d570c1075cb876cf5d65188a633a161b7087d
Cailie.Skelton <CSkelton707@gmail.com>
2020-09-10 14:21:02 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,51 @@
 class Triangle
-  # write code here
+  attr_accessor :s1, :s2, :s3
+  @sides = []
+
+  def initialize(s1, s2, s3)
+    @s1, @s2, @s3 = s1, s2, s3
+    @sides = [s1, s2, s3]
+  end
+
+  def kind()
+    if(negative? == true || valid? == false)
+      raise TriangleError
+    elsif(equilateral?)
+      :equilateral
+    elsif(scalene?)
+      :scalene
+    elsif(isosceles?)
+      :isosceles
+    end
+  end
+
+  def negative?
+    @sides.each do |length|
+      if(length <= 0)
+        return true
+      end
+    end
+  end
+
+  def equilateral?
+    (@s1 == @s2) && (@s1 == @s3)
+  end
+
+  def scalene?
+    (@s1 != @s2) && (@s1!= @s3) && (@s2 != @s3)
+  end
+
+  def isosceles?
+    (@s2 == @s3) || (@s1 == @s2) || (@s1 = @s3)
+  end
+
+  def valid?
+    (@s1 + @s2 > @s3) && (@s2 + @s3 > @s1) && (@s1 + @s3 > @s2)
+  end
+
+
+class TriangleError < StandardError
+
+end
+
 end
